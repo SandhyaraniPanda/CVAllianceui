bf05ae05f45dc45d2e89e75ec9a95bacf01c15bc
SandhyaraniPanda <sandhyaranipanda98@gmail.com>
2018-04-14 19:10:39 +0530

    Initial commit

---
+++ src/app/vendor-regd/vendor-regd.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-vendor-regd',
+  templateUrl: './vendor-regd.component.html',
+  styleUrls: ['./vendor-regd.component.css']
+})
+export class VendorRegdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  OnSubmit(form: NgForm) { }
+
+}
